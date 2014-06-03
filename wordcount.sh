3e507c8fb7d206c35fbaa30a2173875766e634de
Peter E Conn <conn.peter@gmail.com>
2014-06-03 21:14:28 +0100

    Hopefully final commit

--- wordcount.sh
+++ wordcount.sh
@@ -6,15 +6,16 @@ function WordCount {
   detex $1 | wc -w 
 }
 
-back=$(WordCount Background/Background.tex)
-impl=$(WordCount Implementation/Implementation.tex)
-eval=$(WordCount Evaluation/Evaluation.tex)
-total=$(WordCount Dissertation.tex)
-misc=$((eval+impl+back))
-
-printf "Background:     %4d\n" ${back}
-printf "Implementation: %4d\n" ${impl}
-printf "Evaluation:     %4d\n" ${eval}
-echo   "--------------------"
-printf "Total 1:        %4d\n" ${misc}
-printf "Total 2:        %4d\n" ${total}
+WordCount Dissertation.tex
+#back=$(WordCount Background/Background.tex)
+#impl=$(WordCount Implementation/Implementation.tex)
+#eval=$(WordCount Evaluation/Evaluation.tex)
+#total=$(WordCount Dissertation.tex)
+#misc=$((eval+impl+back))
+#
+#printf "Background:     %4d\n" ${back}
+#printf "Implementation: %4d\n" ${impl}
+#printf "Evaluation:     %4d\n" ${eval}
+#echo   "--------------------"
+#printf "Total 1:        %4d\n" ${misc}
+#printf "Total 2:        %4d\n" ${total}
